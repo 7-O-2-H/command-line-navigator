33ecbc1604877e11b2593f288255dc171c6eb62d
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-08 14:34:11 -0400

    add addit styling

--- command-line-navigator/src/pages/index.js
+++ command-line-navigator/src/pages/index.js
@@ -2,7 +2,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import OptionsContainer from "../components/OptionsContainer";
-// import styles from "@/styles/Home.module.css";
 
 
 export default function Home() {
