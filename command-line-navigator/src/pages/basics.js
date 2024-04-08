9100c1244e8516a8fcfe7ebada8683e90999a86e
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-08 14:47:20 -0400

    add basics init tutorial text

--- command-line-navigator/src/pages/basics.js
+++ command-line-navigator/src/pages/basics.js
@@ -7,7 +7,13 @@ export default function Home() {
   return (     
         <div className="basics">
           <Header />
+          <div className="text-box-one" >
+            <p>This is your terminal.</p>
+          </div>
           <Terminal />
+          <div className="text-box">
+            <p> 'What's that?', you might ask. It's an advanced application that allows users - and more commonly developers - to directly communicate with your OS.</p>
+          </div>
         </div>
   );
 }
