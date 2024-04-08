4d74da99f39d51cd4f8d489af0145e41e3141d73
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-04-08 02:48:32 -0400

    correct font size

--- command-line-navigator/src/components/Terminal.jsx
+++ command-line-navigator/src/components/Terminal.jsx
@@ -1,7 +1,31 @@
-export default function SideBar() {
+// import React from 'react';
+
+export default function Terminal() {
+
+  //template
   return (
-        <div className="terminal">
-          
+    <div className="terminal-window">
+      <div className="title-bar">
+        <div className="title-bar-btns">
+          <div className="title-bar-btn close"></div>
+          <div className="title-bar-btn minimize"></div>
+          <div className="title-bar-btn maximize"></div>
         </div>
+        <div className="title">Terminal</div>
+      </div>
+      <div className="content">
+        <div className="terminal">
+          <div className="terminal-header">
+            <div className="terminal-btn red"></div>
+            <div className="terminal-btn yellow"></div>
+            <div className="terminal-btn green"></div>
+          </div>
+          <div className="terminal-body">
+            <p>Welcome to Terminal</p>
+            <p>$ _</p> {/* Placeholder for terminal input */}
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
--- command-line-navigator/src/pages/_app.js
+++ command-line-navigator/src/pages/_app.js
@@ -2,6 +2,8 @@ import "../styles/styles.css";
 import '../styles/header.css';
 import '../styles/optionsContainer.css';
 import '../styles/folder.css';
+import '../styles/terminal.css'; // Import CSS file for styling
+
 
 
 export default function App({ Component, pageProps }) {
